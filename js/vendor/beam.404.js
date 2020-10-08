2c07169f69b30a6a85b6894c2352d45aaf531a3c
johnpilbeam <jpilbeam@gmail.com>
2020-10-08 15:54:32 +0100

    Modified 404 to fix RedirectURL

--- js/vendor/beam.404.js
+++ js/vendor/beam.404.js
@@ -2,7 +2,11 @@ window.onload = function() {
 // Set the variables
 var LastCaptureURL = 'https://wayback.archive-it.org/org-467/20200807110009/';  
 var CurrentPageURL = window.location.href;  
-var RedirectURL = LastCaptureURL + CurrentPageURL;
+var str = CurrentPageURL;
+var LastSBSCaptureURL = str.replace("bodleian", "sbs");
+
+
+var RedirectURL = LastSBSCaptureURL + CurrentPageURL;
 
 // Write to document
 	document.getElementById("beamlink").setAttribute("href",RedirectURL);	
